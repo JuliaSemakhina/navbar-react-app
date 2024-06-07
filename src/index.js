6937b38600a57e6dbb22dbb8882ff47612cdf814
Yuliya Semakhina <47063920+JuliaSemakhina@users.noreply.github.com>
2024-06-07 20:43:44 +0300

    Update index.js
    Deleting lines

--- src/index.js
+++ src/index.js
@@ -6,41 +6,11 @@ import './navbar/index.css';
 import App from './navbar/App'
 import reportWebVitals from './reportWebVitals';
 
-// import '././cocktails/index.css';
-// import App from '././cocktails/App';
-// import reportWebVitals from './reportWebVitals';
-// import { AppProvider } from '././cocktails/context'
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// ReactDOM.render(
-//     <React.StrictMode>
-//       <AppProvider>
-//       <App />
-//       </AppProvider>
-//     </React.StrictMode>,
-//     document.getElementById('root')
-//   );
 
 createRoot(document.getElementById('root'))
 .render(
     <App />
 );
 
-// createRoot(document.getElementById('root'))
-// .render(
-//     <AppProvider>
-//     <App />
-//     </AppProvider>
-// );
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
