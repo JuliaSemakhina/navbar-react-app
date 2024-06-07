81d8e1b302c2817a2d8b031f2f07d2497cc911b0
JuliaSemakhina <contremoi@yandex.ru>
2024-06-07 20:05:59 +0300

    final files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+
+import './navbar/index.css';
+import App from './navbar/App'
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// import '././cocktails/index.css';
+// import App from '././cocktails/App';
+// import reportWebVitals from './reportWebVitals';
+// import { AppProvider } from '././cocktails/context'
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//       <AppProvider>
+//       <App />
+//       </AppProvider>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
+
+createRoot(document.getElementById('root'))
+.render(
     <App />
-  </React.StrictMode>
 );
+
+// createRoot(document.getElementById('root'))
+// .render(
+//     <AppProvider>
+//     <App />
+//     </AppProvider>
+// );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
